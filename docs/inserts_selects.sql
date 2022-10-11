edfa92a93713931e071f7b3459379889faab3da1
lucas <lucastymos@gmail.com>
2022-10-11 16:05:38 -0300

    terminando ajustes banco de dados

--- docs/inserts_selects.sql
+++ docs/inserts_selects.sql
@@ -8,6 +8,8 @@ select * from instituicao;
 select * from uniforme;
 select * from usuario_endereco;
 
+SELECT * FROM unistore.usuario left join usuario_endereco on (usuario.id_usu = usuario_endereco.id_usu) where usuario.id_usu = 1;
+
 -- Inserts
 -- instituicao
 insert into instituicao values ('Fieb');
@@ -18,6 +20,24 @@ insert into instituicao values ('Objetivo');
 insert into instituicao values ('Day Care');
 
 -- usuario
-insert into usuario values ('526.114.628-36','lucas@gmail.com','LucasTymos','1234','(11)978281477')
+insert into usuario(cpf, email,nome, nome_usu, senha,celular) values ('526.114.628-36','lucas@gmail.com','lucas camilo tymoschneko','LucasTymos','1234','(11)978281477');
+
+-- usu_endereco
+insert into usuario_endereco(rua, cep, bairo, cidade, numero, id_usu) values ('Adriano Augusto', '06440-040','Aldeia de Barueri','Barueri','140','1');
 
 -- uniforme
+insert into uniforme(tamanho, condicao,titulo,foto,descricao,cor,valor,contato,nome_instituicao,id_usu) values ('M','nova','Camisa do ITB FIEB','','blablalbblalblb,lbmlbfdembdleb', 'Branca','25','(11)97828-1477','Fieb','1');
+insert into uniforme(tamanho, condicao,titulo,foto,descricao,cor,valor,contato,nome_instituicao,id_usu) values ('G','Seminova','Calça do ITB FIEB','','blablalbblalblb,lbmlbfdembdleb', 'Verde','25','(11)97828-1477','Fieb','1');
+insert into uniforme(tamanho, condicao,titulo,foto,descricao,cor,valor,contato,nome_instituicao,id_usu) values ('P','usada','Camisa do Chalupe','','blablalbblalblb,lbmlbfdembdleb', 'Amarela','45','(11)97828-1477','Chalupe','1');
+insert into uniforme(tamanho, condicao,titulo,foto,descricao,cor,valor,contato,nome_instituicao,id_usu) values ('M','nova','Camisa do Mackenzie','','blablalbblalblb,lbmlbfdembdleb', 'Branca','55','(11)97828-1477','Mackenzie','1');
+insert into uniforme(tamanho, condicao,titulo,foto,descricao,cor,valor,contato,nome_instituicao,id_usu) values ('M','nova','Jaleco','','blablalbblalblb,lbmlbfdembdleb', 'Branca','25','(11)97828-1477','Fieb','1');
+
+
+
+
+
+
+
+
+
+
--- docs/script_BDunistore.sql
+++ docs/script_BDunistore.sql
@@ -2,27 +2,14 @@ create database UniStore;
 
 use Unistore;
 
-create table venda(
-Id_venda int auto_increment, 
-valor_total_venda double, 
-data_venda date,
-status_venda enum('finalizada', 'em andamento', 'disponivel'),
-id_usu int not null,
-PRIMARY KEY (Id_venda),
-FOREIGN KEY(id_usu) REFERENCES usuario(id_usu)
-);
-
-
-
-
-create table compra(
-id_compra int primary key auto_increment, 
-quantidade int, 
-data_pedido date, 
-valor_total_compra double, 
-status_compra enum('finalizada','em andamento'), 
-id_usu int not null,
-FOREIGN KEY (id_usu) REFERENCES usuario(id_usu)
+CREATE TABLE compra (
+  id_compra INT NOT NULL AUTO_INCREMENT,
+  data_pedido DATE NULL DEFAULT NULL,
+  valor_total_compra DOUBLE NULL DEFAULT NULL,
+  status_compra ENUM('finalizada', 'em andamento') NULL DEFAULT NULL,
+  id_usu INT NOT NULL,
+  PRIMARY KEY (id_compra),
+    FOREIGN KEY (id_usu) REFERENCES usuario (id_usu)
 );
 
 create table usuario(
@@ -34,18 +21,7 @@ nome_usu varchar(50),
 senha varchar(20),
 celular varchar(15)
 );
-select * from usuario;
-alter table usuario
-add column celular varchar(15);
 
-create table instituicao (
-nome_instituicao varchar(150) not null primary key, 
-logo longblob, 
-localizacao varchar(300)
-);
-
-insert into instituicao values ('fieb belval','','Belval');
-select * from instituicao;
 
 create table usuario_endereco(
 id_endereco int auto_increment not null primary key,
@@ -58,11 +34,15 @@ id_usu int,
 FOREIGN KEY(id_usu) REFERENCES usuario(id_usu)
 );
 
+create table instituicao (
+nome_instituicao varchar(150) not null primary key
+);
+
 create table uniforme (
 id_produto int not null auto_increment,
 tamanho enum('P', 'M', 'G','infantil') not null, 
 condicao enum('nova', 'seminova', 'usada') not null, 
-titulo varchar(20) not null, 
+titulo varchar(50) not null, 
 foto longblob, 
 descricao varchar(200), 
 cor varchar(30), 
@@ -70,30 +50,22 @@ valor double,
 contato varchar(15),
 nome_instituicao varchar(150) not null, 
 id_usu int,
-Id_venda int,
 primary key (id_produto),
 FOREIGN KEY (nome_instituicao) REFERENCES instituicao(nome_instituicao),
-FOREIGN KEY (id_usu) REFERENCES usuario(id_usu),
-FOREIGN KEY (Id_venda) REFERENCES venda(Id_venda)
+FOREIGN KEY (id_usu) REFERENCES usuario(id_usu)
 );
-select * from uniforme;
-
-drop table uniforme;
 
 
-select * from uniforme;
-insert into uniforme (tamanho, condicao,titulo,foto,descricao,cor,valor,contato,nome_instituicao) values ('M', 'nova', 'Shortinhos', '', 'aaaaaaaaaaaaaaaaaaaaaaaaaaa','Roxo', '50', '(11)978281477','fieb belval');
+CREATE TABLE compra_has_uniforme (
+  compra_id_compra INT NOT NULL,
+  uniforme_id_produto INT NOT NULL,
+  quantidade INT NULL,
+  subtotal DECIMAL(10,2) NULL,
+  PRIMARY KEY (compra_id_compra, uniforme_id_produto),
+FOREIGN KEY (compra_id_compra) REFERENCES compra (id_compra),
+FOREIGN KEY (uniforme_id_produto) REFERENCES uniforme (id_produto)
+    );
 
-SELECT * FROM unistore.usuario left join usuario_endereco on (usuario.id_usu = usuario_endereco.id_usu) where usuario.id_usu = 2;
-
-select * from unistore.usuario where unistore.usuario.id_usu = 1
-union
-select * from unistore.usuario_endereco where unistore.usuario_endereco.id_usu = 1;
-
-insert into usuario values ('1','168.391.360-43', 'lucas@gmail.com', 'lucas', 'lucas2', '1234','(11)978281477');
-select * from usuario;
-insert into usuario_endereco values ('1', 'aaaa aaaaaaa', '06440-467', 'bla bla bla', 'barueri', '145', '1');
-select * from usuario_endereco;
 
 
 
