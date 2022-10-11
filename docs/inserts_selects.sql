dd6e4f8feef9671bfc108cf9e813e84d32d651ab
Lucas Camilo Tymoschenko <lucastymos@gmail.com>
2022-10-11 10:56:48 -0300

    mudanças banco de dados

---
+++ docs/inserts_selects.sql
@@ -0,0 +1,23 @@
+use unistore;
+
+-- selects
+
+select * from usuario;
+select * from compra_has_uniforme;
+select * from instituicao;
+select * from uniforme;
+select * from usuario_endereco;
+
+-- Inserts
+-- instituicao
+insert into instituicao values ('Fieb');
+insert into instituicao values ('Chalupe');
+insert into instituicao values ('Red Balloon');
+insert into instituicao values ('Mackenzie');
+insert into instituicao values ('Objetivo');
+insert into instituicao values ('Day Care');
+
+-- usuario
+insert into usuario values ('526.114.628-36','lucas@gmail.com','LucasTymos','1234','(11)978281477')
+
+-- uniforme
